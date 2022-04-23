da3f0d045e41535934642f600ed02b34ae148172
Marcelo Mota Ribeiro <marcelomotaribeiro@yahoo.com.br>
2022-04-23 18:17:14 -0300

    Ajuste Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-const { resolve } = require('path')
+import { resolve } from 'path'
 
-module.exports = {
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        simple: resolve(__dirname, 'pages/observable-simple/observable-simple.html'),
-        interpolation: resolve(__dirname, 'pages/observable-interpolation/observable-interpolation.html')
-      }
+export const build = {
+  rollupOptions: {
+    input: {
+      main: resolve(__dirname, 'index.html'),
+      simple: resolve(__dirname, 'pages/observable-simple/observable-simple.html'),
+      interpolation: resolve(__dirname, 'pages/observable-interpolation/observable-interpolation.html')
     }
   }
 }
