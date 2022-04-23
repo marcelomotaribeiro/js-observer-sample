8df027468c6b40e0b9cd4ba22c99f02fdb25ac27
Marcelo Mota Ribeiro <marcelomotaribeiro@yahoo.com.br>
2022-04-23 18:31:43 -0300

    Ajuste complementar 2 Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 const { resolve } = require('path')
 
 module.exports = {
-  rollupOptions: {
-    input: {
-      main: resolve(__dirname, 'index.html'),
-      simple: resolve(__dirname, 'pages/observable-simple/observable-simple.html'),
-      interpolation: resolve(__dirname, 'pages/observable-interpolation/observable-interpolation.html')
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        simple: resolve(__dirname, 'pages/observable-simple/observable-simple.html'),
+        interpolation: resolve(__dirname, 'pages/observable-interpolation/observable-interpolation.html')
+      }
     }
   }
 }
